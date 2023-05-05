c4ea689c56f1448eb7f49186f9698641c10e51f5
Nataliya Rieger <artgallery2018@gmx.ch>
2023-05-05 22:38:13 +0200

    update js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,10 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
-export const fetchCountries = (name) => {
-    const URL = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
-    return fetch(URL).then(response => {
-        // console.log(response);
-        if (response.status === 404) {
-            throw new Error(response.status);
-        }
-        // console.log(response.json());
-        return response.json();
-    })  
-};
+export function fetchCountries(name) {
+    return fetch(
+      `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+    ).then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
+  }
